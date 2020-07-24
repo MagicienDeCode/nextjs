42aa1ebba4b35cdc6a1c77a80d2321ae014b4358
Xiang LI <xli@slimpay.com>
2020-07-24 15:53:38 +0200

    init nextjs typescript

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+const Index = () => {
+  return (
+    <div>
+      <h1>
+        <a href="https://github.com/MagicienDeCode" target="_blank">
+          Follow me at Github
+        </a>
+      </h1>
+    </div>
+  );
+};
+
+export default Index;
